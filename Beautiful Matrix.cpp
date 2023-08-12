c592475537752defe0cbeb60c1c949130a05afc7
D-Celest1al <140377014+D-Celest1al@users.noreply.github.com>
2023-08-13 01:10:39 +0600

    Add files via upload

---
+++ Beautiful Matrix.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int x = 0;
+    int a,b;
+
+    for(int i=1;i<=5;i++){
+
+        for(int j=1;j<=5;j++){
+
+            cin>>x;
+
+            if(x==1){
+             a=i;
+             b=j;
+
+            }
+        }
+
+    }
+    int result = abs(a-3)+ abs(b-3);
+    cout<<result<<endl;
+    return 0;
+}
